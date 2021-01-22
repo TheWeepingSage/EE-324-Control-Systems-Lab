292916bd79470a03e528bd0edebb61ac36d8821b
TheWeepingSage <kanabarmillen@gmail.com>
2021-01-22 22:47:30 +0530

    Corrected Lab 2, Q5 code

--- Lab 2/lab_2_q5.m
+++ Lab 2/lab_2_q5.m
@@ -1,17 +1,24 @@
 syms s
+tau = [0.1, 0.5, 2];
+
 num = sym2poly(s+5);
 den_0 = sym2poly((s+2)*(s+4));
 den_1 = sym2poly(s+2);
 den_2=sym2poly(s+4);
-[y_0, t_0] = step(tf(num, den_0));
-[y_1_temp, t_1_temp] = step(tf(num, den_2));
-[y_1, t_1] = lsim(tf(1, den_1), y_1_temp, t_1_temp);
-[y_2_temp, t_2_temp] = step(tf(1, den_1));
-[y_2, t_2] = lsim(tf(num, den_2), y_2_temp, t_2_temp);
-hold on
-plot(t_0, y_0)
-plot(t_1, y_1)
-plot(t_2, y_2)
-legend("Plot 1: Everything Together", "Plot 2: Series, (s+5)/(s+4) first", "Plot 3: Series, Reverse")
-xlabel("Time")
-hold off
+for tau_i=tau
+    t = transpose(0:tau_i:10);
+    y_0 = step(tf(num, den_0), t);
+    y_1_temp = step(tf(num, den_2), t);
+    y_1= lsim(tf(1, den_1), y_1_temp, t);
+    y_2_temp = step(tf(1, den_1), t);
+    y_2 = lsim(tf(num, den_2), y_2_temp, t);
+    figure
+    hold on
+    plot(t, y_0)
+    plot(t, y_1)
+    plot(t, y_2)
+    legend("Plot 1: Everything Together", "Plot 2: Series, (s+5)/(s+4) first", "Plot 3: Series, Reverse")
+    xlabel("Time")
+    title("\tau = " + tau_i)
+    hold off
+end
