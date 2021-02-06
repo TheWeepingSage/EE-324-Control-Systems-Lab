9a6e9e970813b5c4068b9dfef1750e7222d4bb4f
TheWeepingSage <kanabarmillen@gmail.com>
2021-02-06 16:24:13 +0530

    Lab 4, questions 2 and 3 added

---
+++ Lab 4/lab_4_q2.m
@@ -0,0 +1,53 @@
+%---------------------Part a-----------------------------%
+%---------------------------------------------------------%
+sys_g = tf(10, conv([1 0], conv([1 2], [1 4])));
+
+%---------------------Part b-----------------------------%
+%---------------------------------------------------------%
+figure
+hold on
+for k=0:0.1:100
+    sys_series = series(tf(k, 1), sys_g);
+    sys_feedback = feedback(sys_series, 1);
+    poles_sys = pole(sys_feedback);
+    plot(poles_sys, '.')
+end
+xline(0);
+xlabel("Real part")
+ylabel("Imaginary Part")
+hold off
+
+
+
+%---------------------Part c-----------------------------%
+%---------------------------------------------------------%
+for k=0:0.1:100
+    sys_series = series(tf(k, 1), sys_g);
+    sys_feedback = feedback(sys_series, 1);
+    poles_sys = pole(sys_feedback);
+    pole_real_min = min(abs(real(poles_sys) + 0.011435));
+    if pole_real_min <= 0.0001
+        break
+    end
+end
+k
+poles_sys
+figure
+plot(poles_sys, 'o')
+xlabel("Real part")
+ylabel("Imaginary Part")
+xlim([-6.5, 0.5])
+
+%---------------------Part d-----------------------------%
+%---------------------------------------------------------%
+rhStabilityCriterion(cell2mat(sys_feedback.Denominator))
+
+% For K=K_0+0.1
+sys_series_new = series(tf(k+0.1, 1), sys_g);
+sys_feedback_new = feedback(sys_series_new, 1);
+rhStabilityCriterion(cell2mat(sys_feedback_new.Denominator))
+
+% For K=k_0+0.2
+sys_series_new = series(tf(k+0.2, 1), sys_g);
+sys_feedback_new = feedback(sys_series_new, 1);
+rhStabilityCriterion(cell2mat(sys_feedback_new.Denominator))
---
+++ Lab 4/lab_4_q3.m
@@ -0,0 +1,21 @@
+syms s 
+
+fprintf("\n System 1\n")
+poly_1 = s^5 + 3*s^4 + 5*s^3 + 4*s^2 + s + 3;
+poly_1 = sym2poly(poly_1);
+rhStabilityCriterion(poly_1)
+
+fprintf("\n System 2\n")
+poly_2 = s^5 + 6*s^3 + 5*s^2 + 8*s + 20;
+poly_2 = sym2poly(poly_2);
+rhStabilityCriterion(poly_2)
+
+fprintf("\n System 3\n")
+poly_3 = s^5 - 2*s^4 + 3*s^3- 6*s^2 + 2*s - 4;
+poly_3 = sym2poly(poly_3);
+rhStabilityCriterion(poly_3)
+
+fprintf("\n System 4\n")
+poly_4 = s^6 + s^5 - 6*s^4 + s^2 + s - 6;
+poly_4 = sym2poly(poly_4);
+rhStabilityCriterion(poly_4)
