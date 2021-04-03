bb776738c1c60e3ebc5c3f63c378cfa1d5c92e3e
TheWeepingSage <kanabarmillen@gmail.com>
2021-04-04 04:35:48 +0530

    Correct errors in Lab 9

--- Lab 9/lab_9_q1.m
+++ Lab 9/lab_9_q1.m
@@ -2,6 +2,8 @@ sys_g = tf(10, conv([1/5 1 0], [1/20, 1]));
 sys_c_1 = tf([1, 3], [1, 1]);
 sys_c_2 = tf([1, 1], [1, 3]);
 figure
+nyquist(sys_g)
+figure
 nyquist(series(sys_g, sys_c_1))
 figure
 nyquist(series(sys_g, sys_c_2))
